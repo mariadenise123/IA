0b61ed186a6dd1a93c3c8b97a3c4c3f4d79eabbc
mariadenise123 <denise.oliveira.maria@escola.pr.gov.br>
2026-08-13 08:47:52 -0300

    bbbb

--- script.js
+++ script.js
@@ -65,4 +65,16 @@ function mostraPergunta() {
   caixaPerguntas.textContent = perguntaAtual.enunciado;
 }
 
+function mostraAlternativas(){
+  for(const alternativa of perguntaAtual.alternativas) {
+    const botaoAlternativas = document.createElement("button");
+    botaoAlternativas.textContent = alternativa.texto;
+    botaoAlternativas.addEventListener("click",function())
+    atual++;
+    mostraPergunta();
+  })
+    caixaAlternativas.appendChild(botaoAlternativas);
+  }
+}
+
 mostraPergunta();
